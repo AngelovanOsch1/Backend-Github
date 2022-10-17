0162511a431a469c47083b2e323c26d95d99ac4a
AngelovanOsch1 <99641678+AngelovanOsch1@users.noreply.github.com>
2022-10-17 23:17:39 +0200

    level field fixed

--- model/Account.js
+++ model/Account.js
@@ -4,6 +4,12 @@ const { Schema } = mongoose;
 const accountSchema = new Schema({
     username: String,
     password: String,
+    score: {
+        type: Number, default: 0
+    },
+    level: {
+        type: Number, default: 1
+    },
     lastAuthentication: Date,
 });
 
